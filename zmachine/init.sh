428ee5468fa3325f0d17f524cbceb1fdf5bf63f8
Saul Pwanson <saul@pwanson.com>
2013-11-02 21:00:09 -0700

    embedded frotz; minor Makefile enhancement

--- zmachine/init.sh
+++ zmachine/init.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-/bin/busybox mount -t proc proc /proc
-/bin/busybox mount -t sysfs sysfs /sys
-/bin/busybox mdev -s
-/bin/busybox openvt -w -c 1 /bin/sh
+/bin/busybox mount -t devtmpfs devtmpfs /dev
+/bin/busybox mount /dev/hdc /cdrom
+/bin/busybox openvt -w -c 1 /bin/frotz

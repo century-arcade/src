11d570d97c5cddfeb34471bae31506011b2dd226
Saul Pwanson <saul@pwanson.com>
2013-11-20 03:02:12 -0800

    spawn workers to search in parallel; fix endianness issues; better status output

--- tools/vainhash/vainhash.c
+++ tools/vainhash/vainhash.c
@@ -7,12 +7,18 @@
 #include <stdlib.h> // exit
 #include <fcntl.h> // O_RDWR
 #include <getopt.h>
+#include <signal.h>
 
 #include "sha1.h"
 
-#define END_JUNK_SIZE sizeof(uint64_t)
+static volatile int done = 0;
 
-static void hex_print(unsigned char *output, size_t n)
+static void child_exited(int signum)
+{
+    ++done;
+}
+
+static void hex_print(const unsigned char *output, size_t n)
 {
     size_t i;
     for (i=0; i < n; ++i)
@@ -26,22 +32,34 @@ int
 main(int argc, char *const argv[])
 {
     int opt;
-    uint64_t vanityvalue = 0;
+    char vanityvalue[8] = { 0 };
     size_t vanitysize = 2;
-    int nworkers = 1;
+    int num_workers = 1;
+    int force = 0;
 
-    while ((opt = getopt(argc, argv, "p:w:")) != -1) {
+    while ((opt = getopt(argc, argv, "p:w:f")) != -1) {
         switch (opt) {
+        case 'f': // force update even if junk data is non-zero
+            force = 1;
+            break;
+
         case 'p': // hash prefix in hex
         {
             char *endptr = NULL;
-            vanityvalue = strtoull(optarg, &endptr, 16);
+            uint64_t v = strtoull(optarg, &endptr, 16);
             vanitysize = (endptr - optarg) / 2;
+            int i;
+            for (i=vanitysize; i > 0; --i) {
+                vanityvalue[i-1] = v & 0xff;
+                v >>= 8;
+            }
+            fprintf(stderr, "searching for ");
+            hex_print(vanityvalue, vanitysize);
             break;
         }
 
         case 'w': // num workers
-            nworkers = atoi(optarg);
+            num_workers = atoi(optarg);
             break;
 
         default: /* '?' */
@@ -68,7 +86,9 @@ main(int argc, char *const argv[])
     sha1_context *ctx = (sha1_context *) malloc(sizeof(sha1_context));
     sha1_starts(ctx);
 
-    size_t bytes_left = st.st_size - END_JUNK_SIZE;
+    uint64_t junk = 0;
+
+    size_t bytes_left = st.st_size - sizeof(junk);
     while (bytes_left > 0)
     {
         char buf[4096];
@@ -83,19 +103,13 @@ main(int argc, char *const argv[])
         bytes_left -= r;
     }
 
-    char junk[END_JUNK_SIZE];
-
-    if (read(fd, junk, END_JUNK_SIZE) < END_JUNK_SIZE) {
-        perror("could not read junk??");
+    if (read(fd, &junk, sizeof(junk)) < sizeof(junk)) {
+        perror("could not read pre-existing junk??");
         exit(EXIT_FAILURE);
     }
 
-    const char goldzero[END_JUNK_SIZE] = { 0 };
-    assert(memcmp(goldzero, junk, END_JUNK_SIZE) == 0); // must be 0s to start
-
-    // and rewind the junk to eventually overwrite it
-    if (lseek(fd, -END_JUNK_SIZE, SEEK_END) < 0) {
-        perror("lseek");
+    if (!force && junk != 0) {
+        fprintf(stderr, "existing junk is non-zero!  use -f to force\n");
         exit(EXIT_FAILURE);
     }
 
@@ -108,29 +122,66 @@ main(int argc, char *const argv[])
     // show original hash
     unsigned char hash[20];
 
-    sha1_update(ctx, goldzero, END_JUNK_SIZE);
+    sha1_update(ctx, (const unsigned char *) &junk, sizeof(junk));
     sha1_finish(ctx, hash);
 
     fprintf(stderr, "original SHA1 of %s: ", fn);
     hex_print(hash, 20);
 
-    while (1) {
+    // spawn additional workers
+    signal(SIGCHLD, child_exited);
+    pid_t children[1024] = { 0 };
+    int w;
+    for (w=1; w < num_workers; ++w) 
+    {
+        pid_t child = fork();
+        if (child < 0) {
+            perror("fork");
+            break;
+            // don't need to exit, just let the existing thread(s) work
+        } else if (child == 0) { // worker #w
+            break;
+        } else { // parent
+            children[w-1] = child;
+            // continue spawning, then fall through to do our own work
+        }
+    }
+
+    // save start time
+    struct timeval tv_start;
+    gettimeofday(&tv_start, NULL);
+
+    // pre-populate junk (so restarted runs don't re-do the same junk)
+    junk = (tv_start.tv_sec << 32) | tv_start.tv_usec;
+
+    // give each worker its own 56-bit segment of the space.
+    junk &= 0xff00000000000000ULL;
+    junk |= ((uint64_t) w) << 56;
+
+    unsigned long long num_tried = 0;
+
+    while (! done) {
         // restore state
         memcpy(ctx, humblectx, sizeof(sha1_context));
 
         // try some junk
-        sha1_update(ctx, junk, END_JUNK_SIZE);
+        sha1_update(ctx, (const unsigned char *) &junk, sizeof(junk));
         sha1_finish(ctx, hash);
 
         // if the hash begins with the vanity bytes, update the file and quit
         if (memcmp(hash, &vanityvalue, vanitysize) == 0) {
-            fprintf(stderr, "\nwriting junk for vanity hash: ");
-            hex_print(junk, END_JUNK_SIZE);
+            fprintf(stderr, "\n[worker %d] writing junk for vanity hash: ", w);
+            hex_print((const unsigned char *) &junk, sizeof(junk));
 
             fprintf(stderr, "\nfinal hash: ");
             hex_print(hash, 20);
 
-            if (write(fd, junk, END_JUNK_SIZE) < END_JUNK_SIZE) {
+            if (lseek(fd, -sizeof(junk), SEEK_END) < 0) {
+                perror("lseek");
+                exit(EXIT_FAILURE);
+            }
+
+            if (write(fd, &junk, sizeof(junk)) < sizeof(junk)) {
                 perror("junk write");
                 exit(EXIT_FAILURE);
             }
@@ -138,10 +189,32 @@ main(int argc, char *const argv[])
             break;
         }
 
-        unsigned long long iteration = ++(*(uint64_t *) junk);
+        ++num_tried;
+        ++junk;
 
-        if (iteration % 1000000 == 0) {
-            fprintf(stderr, "\r%llu", iteration);
+        if ((junk & 0x00fffff) == (w << 19)) {
+            struct timeval tv_now;
+            gettimeofday(&tv_now, NULL);
+
+            int elapsed_secs = tv_now.tv_sec - tv_start.tv_sec;
+            int elapsed_usecs = tv_now.tv_usec - tv_start.tv_usec;
+            if (elapsed_usecs < 0) {
+                elapsed_secs -= 1;
+                elapsed_usecs += 1000000;
+            }
+            // assume other workers are getting as many done as we are
+            unsigned int rate = 0;
+            
+            if (elapsed_secs > 0)
+                rate = num_tried * num_workers / elapsed_secs;
+
+            fprintf(stderr, "[worker %d] %llu in %ds; %u/s\r", w, num_tried, elapsed_secs, rate);
+        }
+    }
+
+    if (w == num_workers) { // if we're the parent
+        for (w=0; w < num_workers-1; ++w) {
+            kill(children[w], SIGCHLD);
         }
     }
   
